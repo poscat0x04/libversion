ec339e2d20a75c0498a60f96e3cfb93416194d63
Poscat <poscat@mail.poscat.moe>
2021-02-09 23:27:49 +0800

    flake.lock: Update

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
   inputs.flake-utils.url = github:poscat0x04/flake-utils;
 
   outputs = { self, nixpkgs, flake-utils, ... }: with flake-utils;
@@ -8,7 +8,7 @@
         let
           pkgs = nixpkgs.legacyPackages.${system};
           hpkgs = pkgs.haskellPackages;
-          libversion = hpkgs.callCabal2nix "libversion" ./. { };
+          libversion = hpkgs.callCabal2nix "libversion" ./. { libversion = pkgs.libversion; };
           shellDrv = pkgs.haskell.lib.addBuildTools libversion (with pkgs; [
             cabal-install
             haskell-language-server
